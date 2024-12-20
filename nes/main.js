2b76be7451a0df90b0ae604d7082968a2a2131aa
hoanghdtv <hoanghdtv154@gmail.com>
2024-12-20 21:15:55 +0700

    .

---
+++ nes/main.js
@@ -0,0 +1,7 @@
+
+window.onload = function(){
+    var button = document.getElementById('tomtom');
+    // setTimeout(function(){
+    //     button.click();
+    // },1000);  // this will make it click again every 1000 miliseconds
+};
